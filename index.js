44f175ce1ae1b523208dd5cfc5c8d641cd3382e2
chalbrik <paw.swa95@gmail.com>
2024-02-06 21:04:36 +0100

    Update changes

--- index.js
+++ index.js
@@ -29,9 +29,18 @@ const charData = [
   },
 ];
 
+//pobierz dzisiejszą date oraz dzień
+
+const actualDate = new Date();
+let today = actualDate.getDay();
+if (today === 0) {
+  today = 7;
+}
+
 window.onload = () => {
   var highestAmount = 0;
   var heightMultiplyFactor = 0;
+  var createId = 1;
 
   charData.forEach((amountItem) => {
     if (amountItem.amount > highestAmount) {
@@ -43,13 +52,15 @@ window.onload = () => {
   const chartContainerHeight = chartContainer.clientHeight; //pobierz wysokość diva .chart
 
   //stwórz wspólczynik mnożenia tak aby najwyższy słupek wynosił 3/4 wysokości kontenera
-  heightMultiplyFactor = 0.75 * (chartContainerHeight / highestAmount); 
-  charData.forEach((dataItem) => {
+  heightMultiplyFactor = 0.75 * (chartContainerHeight / highestAmount);
 
+  charData.forEach((dataItem) => {
     const chartBarData = document.createElement("div");
-    chartBarData.className = "chart-bar-data"
+    chartBarData.className = "chart-bar-data";
 
     const chartBar = document.createElement("div");
+    chartBar.id = `bar-${createId}`;
+    createId++;
     chartBar.className = "chart-bar";
 
     const chartBarHeight = dataItem.amount * heightMultiplyFactor;
@@ -57,7 +68,7 @@ window.onload = () => {
     chartBar.style.height = `${chartBarHeight}px`;
 
     const chartBarName = document.createElement("p");
-    chartBarName.className = "balance-name font gray"
+    chartBarName.className = "balance-name font gray";
     chartBarName.textContent = dataItem.day;
 
     chartBarData.appendChild(chartBar);
@@ -65,4 +76,9 @@ window.onload = () => {
 
     chartContainer.appendChild(chartBarData);
   });
+
+
+  //zamien kolor dziejszego słupka
+  const prob = document.querySelector(`div#bar-${today}`);
+  console.log(prob);
 };
